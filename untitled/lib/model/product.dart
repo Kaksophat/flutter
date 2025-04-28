0ac62161e64bf88159532378f126a4ef9c350524
Keo Ratana <162092337+ratanakeo2024@users.noreply.github.com>
2025-04-28 10:07:07 +0700

    Update

---
+++ untitled/lib/model/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String imageUrl;
+  final String title;
+  final String description;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+}
